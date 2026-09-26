c1928dc4f5baabaf1136a6fa5d58a2568db5e606
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2026-09-26 23:50:29 +1000

    flush

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.odiousapps.mx3launcher"
         minSdk = 29
         targetSdk = 37
-        versionCode = 47
-        versionName = "0.0.47"
+        versionCode = 48
+        versionName = "0.0.48"
     }
 
     buildTypes {
